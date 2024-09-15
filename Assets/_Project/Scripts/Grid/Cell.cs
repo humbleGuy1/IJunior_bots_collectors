0195977e2bc0a842344a7401643bcead4ac6777e
Veniamin <vrudnevskiy@list.ru>
2024-09-16 00:34:41 +0300

    Add cell

---
+++ Assets/_Project/Scripts/Grid/Cell.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GridSystem
+{
+    public class Cell
+    {
+        public Cell(Vector3 position) =>
+            Position = position;
+
+        public Vector3 Position { get; private set; }
+        public bool IsOccluded { get; private set; }
+    }
+}
+    
+
